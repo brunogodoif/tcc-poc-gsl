363f0037bd6b37ac31926af9f217e11bfbc65ed6
Bruno Godoi <bruno.godoi@itb360.com.br>
2021-09-03 09:37:14 -0300

    Refatoração UC 2 - cálculo de frete

--- modulo-informacoes-cadastrais/app/Http/Controllers/ShippingController.php
+++ modulo-informacoes-cadastrais/app/Http/Controllers/ShippingController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\FreteService;
+use App\Services\ShippingService;
 use Illuminate\Http\Request;
 
 class FreteController extends Controller
 {
     public function calc(Request $request)
     {
-        $FreteService = new FreteService;
-        return $FreteService->caclFrete(
+        $ShippingService = new ShippingService;
+        return $ShippingService->caclFrete(
             $request->get('weight'),
             $request->get('invoice'),
             $request->get('width'),
